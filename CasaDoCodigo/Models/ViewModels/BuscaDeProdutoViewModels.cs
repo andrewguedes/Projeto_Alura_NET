6dfe5bd2d1cea48b54a7fb3fc2d3a40166927cdf
andrewguedes <andrew.guedes.siqueira@gmail.com>
2019-05-05 10:59:40 -0300

    Projeto Alura Net Junior - Atividades add arquivos

---
+++ CasaDoCodigo/Models/ViewModels/BuscaDeProdutoViewModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models.ViewModels
+{
+    public class BuscaDeProdutoViewModels
+    {
+        public IList<Produto> Produtos { get; set; }
+        public string Pesquisa { get; set; }
+
+        public BuscaDeProdutoViewModels(IList<Produto>  produtos)
+        {
+            Produtos = produtos;
+        }
+
+
+    }
+}
---
+++ CasaDoCodigo/Repositories/CategoriaRepository.cs
@@ -0,0 +1,31 @@
+﻿using CasaDoCodigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface ICategoriaRepository
+    {
+        Task SaveCategorias(Categoria categoria);
+    }
+
+    public class CategoriaRepository : BaseRepository<Categoria>, ICategoriaRepository
+    {
+        public CategoriaRepository(ApplicationContext contexto) : base(contexto)
+        {
+        }
+
+        public async Task SaveCategorias(Categoria categoria)
+        {
+            if (!dbSet.Where(c => c.Nome == categoria.Nome).Any())
+            {
+                dbSet.Add(categoria);
+                await contexto.SaveChangesAsync();
+            }
+
+        }
+
+    }
+}
